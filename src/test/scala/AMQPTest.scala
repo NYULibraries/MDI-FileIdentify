c6a16977e9f640c7dab345a707c55e0510375a05
Don Mennerich <don.mennerich@nyu.edu>
2015-12-14 12:49:51 -0500

    adding tests for fido command and test file existence

--- src/test/scala/AMQPTest.scala
+++ src/test/scala/AMQPTest.scala
@@ -11,9 +11,18 @@ import org.json4s.jackson.JsonMethods._
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent._
+import scala.sys.process._
 
 class AMQPSpec extends FlatSpec with Matchers with AMQPTestSupport {
   
+  "A test file" should "exist" in {
+  	assert(testFile() == true)
+  }
+
+  "The fido command" should "be available on the system" in {
+  	assert(testFido == true)
+  }
+
   "A RabbitMQ connection" should "have a heartbeat" in { getHeartbeat() >= 0 should be (true) }
 
   it should "have a channel id" in { getChannel() >= 0 should be (true) }
